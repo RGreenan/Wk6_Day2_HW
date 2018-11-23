b33e0e9773d7c85a2287acdecc8c96f5503347b1
Ray <ray.greenan.89@outlook.com>
2018-11-23 15:54:37 +0000

    Added title & director html form

---
+++ weekend_homework/js/app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+});
+//
+// const createNewForm = function (form) {
+//
+//   const newFormItem = document.createElement('ul');
+//   newFormItem.textcontent = 'ul text';
+//   newFormItem.classList.add('new-form-item');
+//   form.appendChild(newFormItem);
+//
+//   const title = document.createElement('h2');
+//   title.textContent = form.title.value;
+//   newformItem.appendChild(title);
+//
+//   const director = document.createElement('h3');
+//   director.textContent = form.director.value;
+//   newformItem.appendChild(director);
+//
+//   const genre = document.createElement('p');
+//   genre.textContent = form.genre.value;
+//   newformItem.appendChild(genre);
+//
+//   return newFormItem;
+// }
